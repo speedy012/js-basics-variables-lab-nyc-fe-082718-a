5ee3ab48f7f108e569e16ba6e9fd9a3bb26f24af
Herman <hrivas5726@aol.com>
2018-10-02 00:45:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+  it('is set as Scuber', function () {
+      expect(companyName).to.equal('Scuber');
+    });
+
+    it('raises error if the companyName is changed', function () {
+      expect(function () { companyName = 'Scuber' }).to.throw(TypeError);
+    });
+let mostProfitableNeighborhood = 'Chelsea';
+let comapnayCeo = 'Susan Smith';
